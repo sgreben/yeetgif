003606c96d3f23961431e663d1b861436f8fc3fb
Sergey Grebenshchikov <sgreben@gmail.com>
2018-10-21 15:28:39 +0200

    Fix span

--- pkg/floats/span.go
+++ pkg/floats/span.go
@@ -4,8 +4,8 @@ import "math"
 
 func MakeSpan(min, max float64, nPoints int) []float64 {
 	X := make([]float64, nPoints)
-	d := max - min
 	min, max = math.Min(max, min), math.Max(max, min)
+	d := max - min
 	for i := range X {
 		X[i] = min + d*(float64(i)/float64(nPoints-1))
 	}
@@ -13,8 +13,8 @@ func MakeSpan(min, max float64, nPoints int) []float64 {
 }
 
 func Span(min, max float64, X []float64) {
-	d := max - min
 	min, max = math.Min(max, min), math.Max(max, min)
+	d := max - min
 	for i := range X {
 		X[i] = min + d*(float64(i)/float64(len(X)-1))
 	}
